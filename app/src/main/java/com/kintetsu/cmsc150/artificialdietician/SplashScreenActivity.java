1ed088a7cb968b7cf04ed6845ba8dcc7f94b54c9
kintetsu998 <juliusvega998@gmail.com>
2016-10-28 01:29:13 +0800

    Added splash screen

--- app/src/main/java/com/kintetsu/cmsc150/artificialdietician/SplashScreenActivity.java
+++ app/src/main/java/com/kintetsu/cmsc150/artificialdietician/SplashScreenActivity.java
@@ -1,7 +1,9 @@
 package com.kintetsu.cmsc150.artificialdietician;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent  i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        }, 5000);
     }
 }
